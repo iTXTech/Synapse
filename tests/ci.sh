60f6f017824f86efd042383ee90056454985e47a
PeratX <1215714524@qq.com>
2016-07-01 14:16:44 +0800

    Added travis-ci test

---
+++ tests/ci.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+echo Running lint...
+shopt -s globstar
+for file in **/*.php; do
+    OUTPUT=`php -l "$file"`
+    [ $? -ne 0 ] && echo -n "$OUTPUT" && exit 1
+done
+echo Lint done successfully.
+echo -e "version\nms\nstop\n" | php src/synapse/Synapse.php --no-wizard | grep -v "\[Synapse\] Adding "
+if ls plugins/Synapse/Synapse*.phar >/dev/null 2>&1; then
+    echo Server packaged successfully.
+else
+    echo No phar created!
+    exit 1
+fi
